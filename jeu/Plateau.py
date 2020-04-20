625c9854116b4c37ad80fdf42b8aa62bc306d185
Geoffrey SALMON <geoffrey.salmon@std.heh.be>
2020-04-20 13:27:51 +0200

    Update Plateau.py
    Ajout de la méthode SignalerMineAuxVoisins

--- jeu/Plateau.py
+++ jeu/Plateau.py
@@ -15,6 +15,8 @@ liste appel d'autre fonction :
 		- PlacerMines(int Nombre_de_mines)
 	- RemplirCase
 		- Constructeur de la classe Case 
+	- PlacerMines
+		- SignalerMineAuxVoisins
 """
 
 import random
@@ -53,13 +55,39 @@ class Plateau():
 			caseIndex = random.randint(0, self.hauteur * self.largeur - 1) #On choisit aléatoirement une case du plateau
 			while self.cases[caseIndex].mine :  #Tant que la case choisi à déjà une bombe...
 				caseIndex = random.randint(0, self.hauteur * self.largeur - 1) #On recommence
-			ligne = caseIndex // largeur
+			ligne = caseIndex // self.largeur
 			"""
 			La ligne de la case est obtenue en divisant l'index de la case par la largeur du plateau, 
 			soit le nombre de colonnes
 			"""
-			colonne = caseIndex % largeur
+			colonne = caseIndex % self.largeur
 			"""
 			La colonne est donc le reste de la division ci-dessus
 			"""
 			self.cases[caseIndex].mine = true #On passe l'état de la case choisie à miné.
+			self.SignalerMineAuxVoisins(ligne, colonne)
+			
+	def SignalerMineAuxVoisins(self, ligne, colonne):
+		"""
+		Signale la présence d'une mine à tous les voisins de la case
+		
+		Chaque case est représentée comme le croissement d'une ligne et d'une colonne, et par un numéro d'index
+		Chaque index peut être calculée de la manière suivante : Ligne * LargeurPlateau + Colonne.
+		Le plateau se représente da la manière suivante :
+			Chaque colonne est numérotée de 0 à n-1 largeur
+			Chaque ligne est numérotée de 0 à n-1 hauteur
+			Il y a donc (Hauteur * Largeur) Index
+			
+				| C0 | C1 | C2 | C3 |
+			 L0	| 0  | 1  | 2  | 3  |
+			 L1 | 4  | 5  | 6  | 7  |
+			 L2 | 8  | 9  | 10 | 11 |
+			 
+			 Les voisins d'une case en ligne L et colonne C sont donc la partie du tableau allant de
+				- L-1 à L+1
+				- C-1 à C+1
+		"""
+		for L in range(max(0,ligne-1), min(ligne+1, self.hauteur-1)):
+			for C in range(max(0,colonne-1), min(colonne+1, self.largeur-1)):
+				self.cases[L * self.largeur + C].bomsVois += 1
+		
